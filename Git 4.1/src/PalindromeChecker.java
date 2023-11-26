69ecbb3ad9f66065a82ef320e3f6331b586d3b69
Iikka Malmi <Iikka.Malmi@metropolia.fi>
2023-11-26 08:07:35 +0200

    iikka branch + code

--- Git 4.1/src/PalindromeChecker.java
+++ Git 4.1/src/PalindromeChecker.java
@@ -2,14 +2,21 @@ import java.util.Scanner;
 
 public class PalindromeChecker {
 
+    // Main method to test the checker
     public static void main(String[] args) {
 
+        // Prompt for word/phrase and scan it
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word or a phrase: ");
         String input = scanner.nextLine();
 
+        // Call for transform to remove other than letters
         String transformed = transformInput(input);
 
+        // Call to check if transformed input is a palindrome
         boolean isPalindrome = checkPalindrome(transformed);
+
+        // Print based on result
         if (isPalindrome) {
             System.out.println("The transformed input is a palindrome.");
         } else {
@@ -17,13 +24,18 @@ public class PalindromeChecker {
         }
     }
 
+    // Removing everything else than letters and make it lowercase
     public static String transformInput(String input) {
         input = input.toLowerCase();
+        input = input.replace(" ", "");
+        input = input.replace(".", "");
         input = input.replace(",", "");
         input = input.replace("!", "");
+        input = input.replace("?", "");
         return input;
     }
 
+    // Compare reversed string to original
     public static boolean checkPalindrome(String input) {
         String reversed = reverseString(input);
         if (input.equals(reversed)) {
@@ -33,6 +45,7 @@ public class PalindromeChecker {
         }
     }
 
+    // Reverses the string
     public static String reverseString(String input) {
         StringBuilder reversed = new StringBuilder();
         for (int i = input.length() - 1; i >= 0; i--) {
